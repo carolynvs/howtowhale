6f97079976549d4de3219aa3232e35c3eac937fa
carolynvs <me@carolynvanslyck.com>
2016-02-07 09:54:13 -0600

    Use JupyterHub

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # How to Whale
-
-```bash
-docker build -t carolynvs/howtowhale
-docker run --rm -it -p 80:8888 carolynvs/howtowhale
-```
---
+++ hub/jupyterhub_config.py
@@ -0,0 +1,23 @@
+import os
+
+c = get_config()
+
+# Run notebooks in separate container
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+c.DockerSpawner.tls_verify = True
+c.DockerSpawner.tls_ca = "/etc/docker/ca.pem"
+c.DockerSpawner.tls_cert = "/etc/docker/server-cert.pem"
+c.DockerSpawner.tls_key = "/etc/docker/server-key.pem"
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.hub_ip_connect = "howtowhale.com"
+c.DockerSpawner.container_prefix = "howtowhale"
+c.DockerSpawner.container_image = "carolynvs/howtowhale-user"
+
+# Configure oauth
+c.JupyterHub.login_url = "/hub/oauth_login"
+c.Authenticator.admin_users = ["carolynvs"]
+c.JupyterHub.authenticator_class = "oauthenticator.GitHubOAuthenticator"
+c.GitHubOAuthenticator.oauth_callback_url = "http://howtowhale.com/hub/oauth_callback"
+c.GitHubOAuthenticator.client_id = os.environ["GITHUB_CLIENT_ID"]
+c.GitHubOAuthenticator.client_secret = os.environ["GITHUB_CLIENT_SECRET"]
