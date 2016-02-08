cff4bae2422e84c2cf6ca66c0c04cb063cc389ad
carolynvs <me@carolynvanslyck.com>
2016-02-08 15:40:06 -0600

    Remove oauth setup

--- howtowhale-hub/jupyterhub_config.py
+++ howtowhale-hub/jupyterhub_config.py
@@ -15,9 +15,5 @@ c.DockerSpawner.container_prefix = "howtowhale"
 c.DockerSpawner.container_image = "carolynvs/howtowhale-user"
 
 # Configure oauth
-c.JupyterHub.login_url = "/hub/oauth_login"
 c.Authenticator.admin_users = ["carolynvs"]
 c.JupyterHub.authenticator_class = "jupyterhub_carina.CarinaAuthenticator"
-c.CarinaAuthenticator.oauth_callback_url = "http://howtowhale.com/hub/oauth_callback"
-c.CarinaAuthenticator.client_id = os.environ["GITHUB_CLIENT_ID"]
-c.CarinaAuthenticator.client_secret = os.environ["GITHUB_CLIENT_SECRET"]
