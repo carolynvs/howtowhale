c67bf805eb9e853461dc90913a3ee5e35544483a
carolynvs <me@carolynvanslyck.com>
2016-02-14 12:01:46 -0600

    make shit work

--- howtowhale-hub/jupyterhub_config.py
+++ howtowhale-hub/jupyterhub_config.py
@@ -5,11 +5,6 @@ c = get_config()
 # Run notebooks in separate container
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.spawner_class = "jupyterhub_carina.CarinaSpawner"
-c.DockerSpawner.tls_verify = True
-c.DockerSpawner.tls_ca = "/etc/docker/ca.pem"
-c.DockerSpawner.tls_cert = "/etc/docker/server-cert.pem"
-c.DockerSpawner.tls_key = "/etc/docker/server-key.pem"
-c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.hub_ip_connect = "howtowhale.com"
 c.DockerSpawner.container_prefix = "howtowhale"
 c.DockerSpawner.container_image = "carolynvs/howtowhale-user"
