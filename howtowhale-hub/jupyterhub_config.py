35cbf808b6a25251e2be09ca77125776599cc8c0
carolynvs <me@carolynvanslyck.com>
2016-03-19 13:17:20 -0500

    Allow configuring the hub name

--- howtowhale-hub/jupyterhub_config.py
+++ howtowhale-hub/jupyterhub_config.py
@@ -5,15 +5,15 @@ c = get_config()
 # Run notebooks in separate container
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.spawner_class = "jupyterhub_carina.CarinaSpawner"
-c.CarinaSpawner.hub_ip_connect = "howtowhale.com"
+c.CarinaSpawner.hub_ip_connect = "DOMAIN"
 c.CarinaSpawner.container_prefix = "howtowhale"
-c.CarinaSpawner.container_image = "carolynvs/howtowhale-user"
+c.CarinaSpawner.container_image = "carolynvs/howtowhale-user:VERSION"
 c.CarinaSpawner.start_timeout = 300
 c.CarinaSpawner.http_timeout = 300
 
 # Configure oauth
 c.Authenticator.admin_users = ["carolynvs"]
 c.JupyterHub.authenticator_class = "jupyterhub_carina.CarinaAuthenticator"
-c.CarinaAuthenticator.oauth_callback_url = "https://howtowhale.com/hub/oauth_callback"
+c.CarinaAuthenticator.oauth_callback_url = "https://DOMAIN/hub/oauth_callback"
 c.CarinaAuthenticator.client_id = os.environ["CARINA_CLIENT_ID"]
 c.CarinaAuthenticator.client_secret = os.environ["CARINA_CLIENT_SECRET"]
